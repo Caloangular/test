fb836530dbb8aab30f1381f5d398906a41f679c1
Caloangular <caloangular@gmail.com>
2017-05-04 11:22:52 +0200

    Update conf.js

--- src/conf.js
+++ src/conf.js
@@ -2,7 +2,7 @@
 exports.config = {
   directConnect: true,
   framework: 'jasmine',
-  chromeDriver: '../node_modules/chromedriver/lib/chromedriver/chromedriver.exe',
+  chromeDriver: '../node_modules/chromedriver/bin/chromedriver',
   specs: ['spec.js', 'spec2.js'],
   capabilities: {
     browserName: 'chrome'
